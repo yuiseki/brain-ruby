a57d12ab2020ea2c46c3fbb6d20c6942ceea0b3d
yuiseki <yuiseki@gmail.com>
2012-04-20 21:32:51 +0900

    reading from local serial

--- app.rb
+++ app.rb
@@ -5,16 +5,18 @@ require 'sinatra'
 require 'json'
 
 configure do
-  set :public_folder, 'public'
-  disable :protection
+    serial = "/dev/ttyACM1"
+    set :sp, SerialPort.new(serial, 9600)
+    set :public_folder, 'public'
+    disable :protection
 end
 
 get '/brain' do
-  # read brain data from db
+    # read brain data from db
     line = settings.sp.gets
     return Time.now.to_i.to_s+','+line
 end
 
 post '/brain' do
-  # write brain data from db
+    # write brain data from db
 end
